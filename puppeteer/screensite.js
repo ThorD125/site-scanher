a06fda6ec2562307cbd0c6540e1d47e53dd89820
Ther <thor.demeestere@telenet.be>
2022-12-30 14:37:05 +0100

    add u en o args

--- puppeteer/screensite.js
+++ puppeteer/screensite.js
@@ -1,38 +1,8 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const yargs = require('yargs');
 
-const url = process.argv.slice(2)[0];
-
-if (url == undefined) {
-    console.log("No URL provided");
-    process.exit(1);
-}
-
-
-createDir('images');
-
-
-async function run(url) {
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 720 });
-
-    const newurl = checkURL(url);
-
-
-    await page.goto(newurl);
-    await page.waitForNetworkIdle();
-
-    await page.screenshot({
-        path: `images/${newurl.cleanup().replace(/^https?-/g, "")}.png`,
-        fullPage: true,
-    });
-
-    await browser.close();
-}
-
-run(url);
+// const url = process.argv.slice(2)[0];
 
 async function delay(milliseconds) {
     new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -50,12 +20,65 @@ String.prototype.removebaseurl = function () {
 
 
 function createDir(dir) {
-    if (fs.existsSync(dir)) {
-        fs.rmSync(dir, { recursive: true, force: true });
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
     }
-    fs.mkdirSync(dir);
 }
 
 function checkURL(url) {
     return "https://" + `${url.replace(/^https?:\/\//, "")}`;
 }
+
+const argv = yargs
+    .option('url', {
+        alias: 'u',
+        description: 'Input a URL to screenshot',
+        type: 'string'
+    })
+    .option('output', {
+        alias: 'o',
+        description: 'Output folder',
+        type: 'string'
+    })
+    .help()
+    .alias('help', 'h').argv;
+
+if (argv.url == undefined) {
+    console.log("No URL provided");
+    process.exit(1);
+}
+
+if (argv.output == undefined) {
+    argv.output = "./images";
+    createDir(argv.output);
+}
+
+const url = argv.url;
+
+createDir('images');
+const newurl = checkURL(url);
+
+fs.rmSync(`${argv.output}/${newurl.cleanup().replace(/^https?-/g, "")}.png`, { force: true });
+
+async function run(url) {
+
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 720 });
+
+
+
+    await page.goto(newurl);
+    await page.waitForNetworkIdle();
+
+
+    await page.screenshot({
+        path: `${argv.output}/${newurl.cleanup().replace(/^https?-/g, "")}.png`,
+        fullPage: true,
+    });
+
+    await browser.close();
+}
+
+run(url);
