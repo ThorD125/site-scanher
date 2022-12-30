53070178dbacf37aec68519577ea6f58b072830d
Ther <thor.demeestere@telenet.be>
2022-12-30 14:16:08 +0100

    basic url input

--- puppeteer/screensite.js
+++ puppeteer/screensite.js
@@ -1,22 +1,28 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+const url = process.argv.slice(2)[0];
 
-url = "vo-gebruikersbeheer.vlaanderen.be/";
+if (url == undefined) {
+    console.log("No URL provided");
+    process.exit(1);
+}
+
 
 createDir('images');
 
-let browser
 
-async function run() {
+async function run(url) {
     const browser = await puppeteer.launch();
+
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 720 });
 
     const newurl = checkURL(url);
 
+
     await page.goto(newurl);
-    await page.waitForNavigation();
+    await page.waitForNetworkIdle();
 
     await page.screenshot({
         path: `images/${newurl.cleanup().replace(/^https?-/g, "")}.png`,
@@ -26,7 +32,7 @@ async function run() {
     await browser.close();
 }
 
-run();
+run(url);
 
 async function delay(milliseconds) {
     new Promise((resolve) => setTimeout(resolve, milliseconds));
