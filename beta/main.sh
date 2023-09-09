a70c3847cadb115eabdcaa1ff21601376af8033f
ther <thor.demeestere@telenet.be>
2023-09-09 15:41:26 +0200

    f

--- beta/main.sh
+++ beta/main.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 url="example.com"
-# url="hackerone.com"
+url="hackerone.com"
 
 
-sudo subfinder -d $url -o subfinder.txt
+matchResponseCodes="200,301"
+
+
+# subfinder -d $url -o subfinder.txt -nc
+
+cat subfinder.txt | httpx -nc -tech-detect -status-code -title -follow-redirects -o httpx.txt -mc $matchResponseCodes
