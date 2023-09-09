533939914d8ba3cf3487aca7cee67c272f37d9e1
ther <thor.demeestere@telenet.be>
2023-09-09 15:51:48 +0200

    f

--- beta/main.sh
+++ beta/main.sh
@@ -9,4 +9,7 @@ matchResponseCodes="200,301"
 
 # subfinder -d $url -o subfinder.txt -nc
 
-cat subfinder.txt | httpx -nc -tech-detect -status-code -title -follow-redirects -o httpx.txt -mc $matchResponseCodes
+# cat subfinder.txt | httpx -nc -mc $matchResponseCodes -o httpx.txt
+# -tech-detect -status-code -title 
+
+cat httpx.txt | getallurls -o getallurls.txt
