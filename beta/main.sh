4232b49dc39fab36cc1d14b3bd5ca19e2395dc46
ther <thor.demeestere@telenet.be>
2023-09-09 16:09:54 +0200

    f

--- beta/main.sh
+++ beta/main.sh
@@ -7,9 +7,17 @@ url="hackerone.com"
 matchResponseCodes="200,301"
 
 
+# rm subfinder.txt
 # subfinder -d $url -o subfinder.txt -nc
 
-# cat subfinder.txt | httpx -nc -mc $matchResponseCodes -o httpx.txt
-# -tech-detect -status-code -title 
+rm interesting/cnames.txt
+cat subfinder.txt | dnsx -cname -resp -o interesting/cnames.txt
+rm interesting/ipspace.txt
+cat subfinder.txt | dnsx -a -resp-only -o interesting/ipspace.txt
 
-cat httpx.txt | getallurls -o getallurls.txt
+
+# rm httpx.txt
+# cat subfinder.txt | httpx -nc -mc $matchResponseCodes -o httpx.txt
+
+# rm getallurls.txt
+# cat httpx.txt | getallurls -o getallurls.txt
